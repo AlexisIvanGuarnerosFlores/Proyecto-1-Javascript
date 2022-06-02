dbcfc0cf4f8a531ab2bad6774623ddde0cc39f22
Alexis Ivan Guarneros Flores <alexisalekey@gmail.com>
2022-06-01 21:21:28 -0500

    primer repositorio

---
+++ scripts.js
@@ -0,0 +1,117 @@
+// EJERCICIO 1
+// var Bellisimo= prompt ("Eres Bellisimo?")
+// console.log (Bellisimo)
+
+// if (Bellisimo=="si")
+// {
+//     console.log ("Ciertamente")
+// }
+
+// if (Bellisimo=="no")
+// {
+//     console.log("No te creo")
+// }
+
+
+// EJERCICIO 2
+// var Divisible= prompt("Selecciona un numero")
+// console.log(Divisible)
+
+// if(Divisible%2==0)
+// {console.log(Divisible + ' ' +"es un numero divisible entre dos")}
+
+
+// else
+// {console.log(Divisible + ' ' +"no es un numero divisible entre dos")}
+
+
+// // EJERCICIO 3
+// var NumeroPar= prompt ("Selecciona un número")
+// // console.log (NumeroPar)
+
+// if (NumeroPar%2==0)
+// {console.log(NumeroPar + ' ' +"es un numero par")}
+
+// else {console.log(NumeroPar + ' ' +"es un numero impar")}
+
+
+// EJERCICIO 4
+// var Premio =prompt("selecciona un número")
+
+// if (Premio==1000) {console.log("ganaste un Premio")}
+
+// else {console.log("sigue participando")}
+
+
+
+
+
+
+// // EJERCICIO 5
+// var Numero1 = prompt("Ingresa el primer número");
+// var Numero2 = prompt("Ingresa el segundo número");
+
+
+// if (Numero1<Numero2) {
+//     alert("el" + Numero1 + "es menor")
+// }
+
+// else
+// { alert("el" + Numero2 + "es menor") }
+
+// EJERCICIO 6
+
+
+
+
+// EJERCICIO 7
+
+var Num1= prompt("ingresa el primer numero")
+var Num2= prompt("ingresa el segundo numero")
+var Num3= prompt("ingresa el tercero numero")
+
+if (Num1>Num2 && Num1>Num3)
+{console.log(Num1+' '+"Es el numero mayor")}
+
+else if (Num2>Num1 && Num2>Num3)
+{console.log(Num2+ ' '+"Es el numero mayor")}
+else (Num3>Num1 && Num3>Num2)
+{ console.log(Num3+ ' '+"Es el numero mayor")}
+
+
+ // EJERCICIO 8
+// var Calificacion= prompt("Ingresa tu calificacion");
+
+// if (Calificacion> 10)
+// {alert ("error")}
+
+// else if (Calificacion== 10)
+// {alert ("excelente")}
+
+// else if (Calificacion== 9)
+// {alert ("bien")}
+
+// else if (Calificacion>= 6 && Calificacion<= 8)
+// {alert ("Regular")}
+
+// else if (Calificacion<= 5)
+// {alert ("Reprobado")}
+
+
+
+// EJERCICIO 9
+
+// var Helado= prompt("Elige tu helado")
+
+// if (Helado=="Helado sin topping")
+// {alert ("cuesta $50")}
+
+// if (Helado=="Helado topping de oreo")
+// {alert ("cuesta $10")}
+// if (Helado=="Helado topping kitKat")
+// {alert ("cuesta $15")}
+// if (Helado=="Helado topping brownie")
+// {alert ("cuesta $20")}
+// else
+// {alert ("Escribe correctamente el tipo de helado")}
+
